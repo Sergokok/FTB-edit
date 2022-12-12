520a6557e961f755829b6345067b3d0ac0fda839
User <Sergikov 108997261+sergikov@users.noreply.github.com>
2022-12-12 13:29:44 +0300

    fix: возврат со стр 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,12 +3,21 @@ import { useHistory } from "react-router-dom";
 import "./NotFound.css";
 
 function NotFound() {
-    const history = useHistory();
+    // const history = useHistory();
+    function goBack() {
+        window.history.go(-3);
+    }
+
     return (
         <section className="not-found">
             <h1 className="not-found__title">404</h1>
             <p className="not-found__subtitle">Страница не найдена</p>
-            <button className="not-found__button" onClick={() => history.goBack()}>Назад</button>
+            <button
+                className="not-found__button"
+                // onClick={() => history.goBack()}
+                onClick={goBack}
+            >Назад
+            </button>
         </section>
     )
 }
